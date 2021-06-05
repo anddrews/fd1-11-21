d66256115b79c75c84f91f2d676cdce80990a9e6
andrei.kandratsenka <anddrews@tut.by>
2021-06-05 11:52:15 +0300

    asnc deffer

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 //here will by JavaScript code ...
+// alert('Hello world');
+new Splide( '.test', {
+    type: 'loop',
+      perPage: 2,
+    speed: 2000,
+    gap: '20px',
+    start: 3,
+    interval: '100'
+  } ).mount();
