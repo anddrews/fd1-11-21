daedc0ae3cabb8679e976b1a9cbe91b2f99c38a0
andrei.kandratsenka <anddrews@tut.by>
2021-06-12 10:12:08 +0300

    DOM

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,14 @@
 // console.log('console.log');
 
 // boolean operartors AND (&&) OR (||)  NOT (!)
+console.log('fnFoo(2, 3) = ', fnFoo(10, 10));
+// console.log(notExist);
+console.log('variable a ', a);
 
+// fnBoo();
 var a = false;
+console.log('variable a ', a);
+
 var b = true;
 
 console.log('a = false b = true');
@@ -90,7 +96,8 @@ console.log(tmp);
 // }
 
 var i = 0;
-for(; ;i++) {
+
+for(i = 5; i < 10; i++) {
     if (i === 4) {
         continue;
     }
@@ -101,3 +108,98 @@ for(; ;i++) {
    }
 }
 
+i = 10;
+while (i < 10) {
+    console.log('while ', i++);
+};
+
+do {
+    console.log('do while ', i++);
+} while (i < 10 );
+
+var doo = 'doo above function;'
+console.log(' foo above function ', foo);
+function fnFoo(param1, param2) {
+    console.log('doo in function ', doo);
+    var boo = param1 * 2;
+    var foo = param2 * 2;
+    console.log('foo in function ', foo);
+
+    return boo + foo;
+}
+
+console.log('fnFoo(2, 3) = ', fnFoo(2, 3));
+console.log('"fnFoo(3, 3) = "', fnFoo(3, 3));
+
+(function() {
+    console.log('anonimus function');
+})();
+
+var fnBoo = function innerName() {
+    console.log('function expression');
+};
+
+fnBoo();
+// 1 3 
+function fibonnachi(n) {
+    if (n === 2) {
+        return 1;
+    }
+    if (n <= 1) {
+        return 0;
+    }
+    return fibonnachi(n - 1) + fibonnachi(n - 2);
+
+}
+
+for (var i = 1; i < 10; i++) {
+    console.log(fibonnachi(i));
+}
+
+
+var obj = {
+    green: 'yellow',
+    '123kebab-case': 'kebab-case',
+    tmp: {
+        fn: {
+            tmp: function() {
+                console.log('Somewhere from deep');
+            }
+        }
+    }
+};
+
+obj.tmp.fn.tmp();
+
+var tmp = document.querySelector('p');
+tmp.classList.add('warning');
+
+var foo = document.querySelectorAll('p');
+
+foo.forEach(function(item) {
+    item.classList.add('warning');
+})
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+
+console.log('*\n**\n***\n****\n*****');
+console.log('    *\n   ***\n  *****\n *******\n*********');
+// console.log('**');
+// console.log('***');
+// console.log('****');
+// console.log('*****');
+// console.log('******');
+
+tmp.innerHTML = '    *\n   ***\n  *****\n *******\n*********';
